9745ce35e228f9ab57131035beab06b88fdadbd3
Varun Agrawal <varagrawal@gmail.com>
2016-03-02 12:19:22 -0500

    added cool Java POCs

--- Java/RobotTest.java
+++ Java/RobotTest.java
@@ -2,31 +2,31 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 
-public class RobotTest{
+public class RobotTest {
 
-	public static void type(Robot robo, int key){
-		robo.keyPress(key);
-		robo.keyRelease(key);
-		robo.delay(1000);
-	}
-	
-	public static void main( String[] args ) throws Exception{
-		
-		Robot robo = new Robot();
+    public static void type(Robot robo, int key) {
+        robo.keyPress(key);
+        robo.keyRelease(key);
+        robo.delay(1000);
+    }
 
-		robo.mouseMove(200, 200);
+    public static void main(String[] args) throws Exception {
 
-		robo.delay(5000);
+        Robot robo = new Robot();
 
-		RobotTest.type(robo, KeyEvent.VK_V);
-		RobotTest.type(robo, KeyEvent.VK_A);
-		RobotTest.type(robo, KeyEvent.VK_R);
-		RobotTest.type(robo, KeyEvent.VK_U);
-		RobotTest.type(robo, KeyEvent.VK_N);
-		RobotTest.type(robo, KeyEvent.VK_SPACE);
-		RobotTest.type(robo, KeyEvent.VK_R);
-		RobotTest.type(robo, KeyEvent.VK_O);
-		RobotTest.type(robo, KeyEvent.VK_X);
- 	 	    
-	}
+        robo.mouseMove(200, 200);
+
+        robo.delay(5000);
+
+        RobotTest.type(robo, KeyEvent.VK_V);
+        RobotTest.type(robo, KeyEvent.VK_A);
+        RobotTest.type(robo, KeyEvent.VK_R);
+        RobotTest.type(robo, KeyEvent.VK_U);
+        RobotTest.type(robo, KeyEvent.VK_N);
+        RobotTest.type(robo, KeyEvent.VK_SPACE);
+        RobotTest.type(robo, KeyEvent.VK_R);
+        RobotTest.type(robo, KeyEvent.VK_O);
+        RobotTest.type(robo, KeyEvent.VK_X);
+
+    }
 }
